7c2a111ca17ef416fc675309e9e150e00870efe2
Ritik Saxena <123720012+ritik91625@users.noreply.github.com>
2024-04-24 00:59:02 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 22:59:52 2024
+
+@author: Ritik
+"""
+
+import streamlit as st
+st.image("C:/Users/Ritik/3D Objects/Health Guard System/Saved_models/a.jpg")
